67d478837fa746294d0bb2bff7921282729ad733
Gueguen Ronan <37030663+Kairides@users.noreply.github.com>
2020-05-08 14:29:24 +0200

    Validation plug-in for the Ale meta-programming approach in GEMOC Studio (#34)
    * Validation plug-in added to Ale engine
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * Plug-in files moved
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * Validation plug-in added to the plugin.xml
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * Pom.xml added to the validation plug-in
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * ArtifactId changed in pom.xml
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * ArtifactId corrected
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * Validation plug-in added to feature.xml
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * packaging changed to 'eclipse-plugin'
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * Plug-in ID correted
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * Plugin version changed
    
    Signed-off-by: Ronan Guéguen <gueguen.ronan1@gmail.com>
    
    * Severity changed for the @init tag check
    
    Signed-off by Ronan Guéguen <gueguen.ronan1@gmail.com>

---
+++ plugins/org.eclipse.gemoc.ale.language.metaprogramming/src/org/eclipse/gemoc/ale/language/metaprogramming/AleRule.java
@@ -0,0 +1,104 @@
+package org.eclipse.gemoc.ale.language.metaprogramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecoretools.ale.Operation;
+import org.eclipse.emf.ecoretools.ale.Tag;
+import org.eclipse.emf.ecoretools.ale.Unit;
+import org.eclipse.gemoc.dsl.Dsl;
+import org.eclipse.gemoc.dsl.Entry;
+import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.IRule;
+import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.Message;
+import org.eclipse.gemoc.xdsmlframework.api.extensions.metaprog.Severity;
+
+/**
+ * Validation rule used by the Ale meta-programming approach
+ * 
+ * @author GUEGUEN Ronan
+ *
+ */
+public class AleRule implements IRule{
+
+	@Override
+	public Message execute(Dsl dsl) {
+		
+		ArrayList<String> entriesNames = new ArrayList<String>();
+		
+		for (Entry e : dsl.getEntries()) {
+			entriesNames.add(e.getKey());
+		}
+		
+		if(!entriesNames.contains("ale")) {
+			return (new Message("Missing entry \"ale\"", Severity.ERROR));
+		}
+			
+		return (new Message("",Severity.DEFAULT));
+	}
+
+	@Override
+	public Message execute(Entry entry) {
+		if("ale".matches(entry.getKey())) {
+			URI uri = URI.createURI(entry.getValue());
+			
+			if(!uri.isPlatformResource()) {
+				return (new Message("File for \"ale\" entry not in the workspace", Severity.ERROR));
+			}
+						
+			ResourceSet rs = new ResourceSetImpl();
+			Resource res;
+			
+			try {
+				
+				res = rs.getResource(uri, true);
+			
+				List<EObject> contents = res.getContents().get(0).eContents();				
+				
+				if(contents.isEmpty()) {
+					return (new Message("No classes in ale file", Severity.ERROR));
+				}	
+				
+				TreeIterator<EObject> tree = res.getAllContents();
+				List<Tag> tags = new ArrayList<Tag>();
+				while(tree.hasNext()) {
+					EObject node = tree.next();
+					if(node instanceof Unit) {
+						// TODO: If I have the time, i should perform a check on the behaviour name
+						// Unit nodeUnit = (Unit) node;
+					}
+					if(node instanceof Operation) {
+						Operation nodeOperation = (Operation) node;
+						if(!nodeOperation.getTag().isEmpty()) {
+							tags.addAll(nodeOperation.getTag());
+						}
+					}
+				}
+				ArrayList<String> tagNames = new ArrayList<>();
+				
+				for(Tag tag : tags) {
+					tagNames.add(tag.getName());
+				}
+				
+				if(!tagNames.contains("init")) {
+					return (new Message("The Ale file does not contain an \"@init\" operation", Severity.WARNING));
+				}
+				
+				if(!tagNames.contains("main")) {
+					return (new Message("The Ale file does not contain an \"@main\" operation", Severity.ERROR));
+				}			
+			
+				
+			}catch (RuntimeException e) {
+				return (new Message("The file for the \"ale\" entry does not exist", Severity.ERROR));
+			}
+		}
+		return (new Message("", Severity.DEFAULT));
+	}
+
+}
